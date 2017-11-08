2b028fa2224600874d9dd2691c86554e505d888c
73174 <julian07027@gmail.com>
2017-11-08 16:04:07 +0800

    Modified hello.py

--- flask_project/hello.py
+++ flask_project/hello.py
@@ -3,9 +3,21 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/user/admin')
+def show_user_profile(username):
+    return 'User %s' % username
+
+@app.route('/post/7777')
+def show_post(post_id):
+    return 'Post %d' % post_id
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
