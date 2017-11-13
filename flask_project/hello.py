a7540afebc5549960650f7e3ccdf2e391a2a0d3e
meliodaseren-MacOSX <julian07027@gmail.com>
2017-11-13 14:22:16 +0800

    Move template to flask project

--- flask_project/hello.py
+++ flask_project/hello.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+# Routing
+
 @app.route('/')
 def index():
     return 'Index Page'
@@ -10,6 +13,9 @@ def index():
 def hello_world():
     return 'Hello World!'
 
+
+# Variable Rules
+
 @app.route('/user/admin')
 def show_user_profile(username):
     return 'User %s' % username
@@ -17,6 +23,19 @@ def show_user_profile(username):
 @app.route('/post/7777')
 def show_post(post_id):
     return 'Post %d' % post_id
+
+
+# Unique URLs / Redirection Behavior
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about/')
+def about():
+    return 'The about page'
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
